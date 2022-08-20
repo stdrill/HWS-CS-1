511b09161f5de95d920397f2477c24548658ada9
stdrill <tanya23.96@mail.ru>
2022-08-20 20:54:54 +0300

    Task 8 added

---
+++ task8/Program.cs
@@ -0,0 +1,6 @@
+﻿string number = Convert.ToString(new Random().Next(100, 999));
+Console.WriteLine(number);
+string number2 = Convert.ToString(number[0] + "" + number[2]);
+Console.WriteLine(number2);
+
+
