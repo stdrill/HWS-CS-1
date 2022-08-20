95b7572426d3336ce8597a2825db5001d74cfc51
stdrill <tanya23.96@mail.ru>
2022-08-20 22:11:46 +0300

    Task 10 added

---
+++ task10/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(1,999);
+Console.WriteLine(number);
+if (number < 100) {
+    Console.WriteLine("Третьей цифры нет");
+} else {
+    Console.WriteLine(number % 10);
+}
