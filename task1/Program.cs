bd0bb44f0684fe787ab5493b8065396bb46b2893
stdrill <tanya23.96@mail.ru>
2022-08-20 14:11:20 +0300

    Task 1 added

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = b * b;
+if (a == c) {
+    Console.WriteLine("Первое число является квадратом второго");
+} else {
+    Console.WriteLine("Первое число не является квадратом второго");
+}
