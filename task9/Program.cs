f2c5f34cd8086176476a88d11172ce349ce634a6
stdrill <tanya23.96@mail.ru>
2022-08-20 22:41:56 +0300

    Task 9 beautified

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,10 +1,12 @@
-﻿int number = new Random().Next(100, 999);
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int number = new Random().Next(100, 999);
 Console.WriteLine(number);
-Console.WriteLine("Введите число ");
+Console.Write("Задайте число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 if (number % a == 0) {
     Console.WriteLine("Число кратно заданному");
 } else {
-    Console.WriteLine("Число не кратно заданному, остаток от деления: ");
-    Console.Write(number % a);
+    Console.Write("Число не кратно заданному, остаток от деления: ");
+    Console.WriteLine(number % a);
 }
