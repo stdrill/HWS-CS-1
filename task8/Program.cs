a26670e745eb7282bf6a0dcf6a48223ca201756b
stdrill <tanya23.96@mail.ru>
2022-08-20 22:38:39 +0300

    Task 8 beautified

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,4 +1,6 @@
-﻿string number = Convert.ToString(new Random().Next(100, 999));
+﻿// Удалить вторую цифру трёхзначного числа
+
+string number = Convert.ToString(new Random().Next(100, 999));
 Console.WriteLine(number);
 string number2 = Convert.ToString(number[0] + "" + number[2]);
 Console.WriteLine(number2);
