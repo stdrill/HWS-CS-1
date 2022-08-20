934b3ffce92316602a566c288f8c7e0c246bf272
stdrill <tanya23.96@mail.ru>
2022-08-20 15:22:05 +0300

    Task 4 added

---
+++ task4/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0) {
+    Console.WriteLine("Число является четным");
+} else {
+    Console.WriteLine("Число является нечетным");
+}
+
