2238bbea9a384df8c8083716565f4a47203de725
stdrill <tanya23.96@mail.ru>
2022-08-20 22:25:39 +0300

    Task 2 beautified

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,6 +1,9 @@
-﻿string[] arrayWeek = {"Понедельник","Вторник","Среда","Четверг","Пятинца","Суббота","Воскресенье"};
+﻿// По заданному номеру дня недели вывести его название
+
+string[] arrayWeek = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+Console.Write("Введите порядковый номер: ");
 int numWeek = Convert.ToInt32(Console.ReadLine());
-if (numWeek < arrayWeek.Length && numWeek > 0){
+if (numWeek <= arrayWeek.Length && numWeek > 0){
     Console.WriteLine(arrayWeek[numWeek - 1]);
 } else {
     Console.WriteLine("Введеное число не совпадает с днем недели");
