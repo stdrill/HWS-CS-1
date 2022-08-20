332d9939382d36dfb3e1a85d269b6eb911a4b1c7
stdrill <tanya23.96@mail.ru>
2022-08-20 22:20:59 +0300

    Task 1 beautified

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,9 @@
-﻿int a = int.Parse(Console.ReadLine());
-int b = int.Parse(Console.ReadLine());
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
 int c = b * b;
 if (a == c) {
     Console.WriteLine("Первое число является квадратом второго");
