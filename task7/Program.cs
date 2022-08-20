a9c0379406a2d45fec8a314b743937479da3d2a5
stdrill <tanya23.96@mail.ru>
2022-08-20 16:28:57 +0300

    Task 7 added

---
+++ task7/Program.cs
@@ -0,0 +1,9 @@
+﻿string number = Convert.ToString(new Random().Next(10,99));
+Console.WriteLine(number);
+if (number[0] == number[1]) {
+    Console.WriteLine("Оба числа равны "+number[0]);
+} else if (number[0] > number[1]) {
+    Console.WriteLine(number[0]);
+} else {
+    Console.WriteLine(number[1]);
+}
