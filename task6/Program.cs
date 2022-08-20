75e84a5cb96e7e798cb77a09bdf10169a918d4dc
stdrill <tanya23.96@mail.ru>
2022-08-20 22:35:42 +0300

    Task 6 beautified

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,3 +1,5 @@
-﻿string number = Convert.ToString(new Random().Next(100, 999));
+﻿// Показать вторую цифру трёхзначного числа
+
+string number = Convert.ToString(new Random().Next(100, 999));
 Console.WriteLine(number);
 Console.WriteLine(number[1]);
