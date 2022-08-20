55aaa97d0f1c95253170f36ac287d8d62da610d3
stdrill <tanya23.96@mail.ru>
2022-08-20 15:02:00 +0300

    Task 3 added

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = new Random().Next(1, 50);
+int b = new Random().Next(1, 50);
+int c = new Random().Next(1, 50);
+Console.WriteLine("{0} {1} {2}", a, b, c);
+int max = a;
+if (b > max) {
+    max = b;
+} else if (c > max) {
+    max = c;
+}
+Console.WriteLine(max);
