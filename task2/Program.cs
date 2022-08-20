66cc795c9f944b495f0945e6ce785b905f800dab
stdrill <tanya23.96@mail.ru>
2022-08-20 14:42:29 +0300

    Task 2 added

---
+++ task2/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] arrayWeek = {"Понедельник","Вторник","Среда","Четверг","Пятинца","Суббота","Воскресенье"};
+int numWeek = Convert.ToInt32(Console.ReadLine());
+if (numWeek < arrayWeek.Length && numWeek > 0){
+    Console.WriteLine(arrayWeek[numWeek - 1]);
+} else {
+    Console.WriteLine("Введеное число не совпадает с днем недели");
+}
